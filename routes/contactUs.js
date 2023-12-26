32550608bce20a366cf035605d6a26b06c6008a2
Himanshu Sah <himanshusah41@gmail.com>
2023-12-26 13:12:01 -0500

    Pages added

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.MAIL_USERNAME,
-      pass: process.env.MAIL_PASSWORD,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN
-    }
-  });
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       type: 'OAuth2',
+//       user: process.env.MAIL_USERNAME,
+//       pass: process.env.MAIL_PASSWORD,
+//       clientId: process.env.OAUTH_CLIENTID,
+//       clientSecret: process.env.OAUTH_CLIENT_SECRET,
+//       refreshToken: process.env.OAUTH_REFRESH_TOKEN
+//     }
+//   });
   
 router.post('/', (req,res)=>{
     console.log(req.body);
